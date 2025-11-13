8947f1b19605ed26fa71bc6b4902ecffee0ffce9
rt3 <r.talarico3@studenti.unipi.it>
2025-11-13 10:16:06 +0100

    Expanded linguistic_utils

--- task1/linguistic_utils.py
+++ task1/linguistic_utils.py
@@ -2,15 +2,22 @@ import re
 drug_words = {
     # English
     "weed", "cocaine", "crack", "heroin", "joint", "blunt", "acid",
-    "lsd", "meth", "ecstasy", "molly", "dope", "high",
+    "lsd", "meth", "ecstasy", "molly", "dope", "high", "purple", "pusher", "coke",
+    "fentanil"
     # Italian
     "droga", "fumo", "spinello", "erba", "canne", "cocaina", "eroina", "coca", "bianca",
-    "lean", "md"
+    "lean", "md", "fatto", "metanfetamina", "canna", "spinelli", "trip","abuso","sostanza",
+    "sostanze","acidi","acido","pere","ago", "anfetamina", "assenzio","astinenza","ayahuasca",
+    "bamba", "benzo", "benzodiazepine", "xanax", "xanny", "goccie", "goccia", "cannone", "bomba",
+    "narghilè", "botta", "busta", "roba", "spaccio", "spacciare", "calo", "calarsi", "cala", 
+    "cartone", "keta", "ketamina", "dipendenza", "dipendente", "dose", "funghetti", "hashish",
+    "pasticca", "pasticche", "tossico", "metadone", "neve", "perquisa","pippo", "pippare","pippato",
+    "popper", "rollo", "rollare", "sbronza", "sbronzo", "scitto", "sniffare", "sniffo"
 }
 violence_words = {
-    "uccidere", "ammazzare", "sparare", "pistola", "arma", "coltello",
-    "violenza", "massacro", "pugno", "botte", "omicidio", "sangue",
-    "morte", "colpire", "ucciso", "freddo", "freddare"
+    "uccidere", "uccido", "ammazzare", "ammazzo", "sparare", "sparo", "sparerò", "pistola", "arma", "coltello",
+    "violenza", "massacro", "pugno", "botte", "omicidio", "sangue", "tagliare", "taglio",
+    "morte", "colpire", "ucciso", "freddo", "freddare", "morto", "coltello", "lama", "sbirro"
 }
 
 
